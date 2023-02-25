bd68f668ac07d095fe98aefbbae307f812d8980a
PavelElenov <pavelelenov@gmail.com>
2023-02-25 21:36:42 +0200

    Create home page

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,12 +2,13 @@ const menu = document.getElementsByClassName("menu")[0];
 menu.addEventListener("click", () => {
     const navigationBar = document.querySelector(".navigation-bar");
     const menuIcon = document.querySelector(".menu i");
+    const isActive = navigationBar.className.includes("active-navigation-bar");
 
-    if(navigationBar.style.display == "block"){
-        navigationBar.style.display = "none";
+    if(isActive){
+        navigationBar.className = navigationBar.className.replace(" active-navigation-bar", "");
         menuIcon.className = "fa-solid fa-bars";
     }else{
-        navigationBar.style.display = "block";
+        navigationBar.className += " active-navigation-bar";
         menuIcon.className = "fa-solid fa-xmark"
     }
 })
